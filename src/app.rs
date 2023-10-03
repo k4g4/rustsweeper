860172cefdf166778024a252899e7a3c05b2ff83
Andres Dejesus <andresdejesus123@gmail.com>
2023-10-02 20:41:48 -0500

    dark theme toggle button

--- src/app.rs
+++ src/app.rs
@@ -6,12 +6,19 @@ use leptos_router::*;
 use crate::app_error::AppError;
 use crate::pages::{Error, Game, HomePage};
 
-// #[cfg(feature = "ssr")]
-// async fn get_theme() {
-//     use axum_extra::extract::cookie::{CookieJar, Cookie};
-//     use leptos_axum::extract;
+const LIGHTBULB_SVG: &str = include_str!("../svgs/lightbulb.svg");
+const MOON_SVG: &str = include_str!("../svgs/moon.svg");
 
-// }
+#[cfg(feature = "ssr")]
+async fn get_theme() {
+    use axum_extra::extract::cookie::{CookieJar, Cookie};
+    use leptos_axum::extract;
+
+    extract(|jar: CookieJar| async move {
+        jar
+    })
+    .await;
+}
 
 #[component]
 pub fn App() -> impl IntoView {
@@ -43,6 +50,25 @@ pub fn App() -> impl IntoView {
             .into_view()
         }>
             <main>
+                <button
+                    class="theme-toggle"
+
+                    on:click=move |_| {
+                        set_theme((if theme() == "light" { "dark" } else { "light" }).to_string())
+                    }
+
+                    inner_html=move || {
+                        match theme().as_str() {
+                            "light" => {
+                                MOON_SVG
+                            }
+                            "dark" => {
+                                LIGHTBULB_SVG
+                            }
+                            _ => { "" }
+                        }
+                    }
+                />
                 <Routes>
                     <Route path="" view=HomePage />
                     <Route path="game" view=Game />
