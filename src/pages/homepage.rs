2c2109998816752c5dbe5e5c9a5fb0f9c6a55b8f
Andres Dejesus <andresdejesus123@gmail.com>
2023-11-03 22:14:34 -0500

    random name button

--- src/pages/homepage.rs
+++ src/pages/homepage.rs
@@ -8,6 +8,7 @@ use rand::seq::SliceRandom;
 use crate::app_settings::{apply_setting, fetch_setting, Difficulty, Size};
 
 const USERNAME_BOUNDS: RangeInclusive<usize> = 3..=10;
+const DICE_SVG: &str = include_str!("../../svgs/dice.svg");
 
 fn valid_chars(username: &str) -> bool {
     username
@@ -15,13 +16,17 @@ fn valid_chars(username: &str) -> bool {
         .all(|c| c.is_ascii_alphabetic() || c == '_')
 }
 
+fn random_name() -> String {
+    include!("../../names.json")
+        .choose(&mut rand::thread_rng())
+        .expect("array is nonempty")
+        .to_string()
+}
+
 /// Renders the home page.
 #[component]
 pub fn HomePage() -> impl IntoView {
-    let (username, set_username) = create_signal(fetch_setting("username").unwrap_or_else(|| {
-        let names = include!("../../names.json");
-        names.choose(&mut rand::thread_rng()).unwrap().to_string()
-    }));
+    let (username, set_username) = create_signal(fetch_setting("username"));
     let (difficulty, set_difficulty) =
         create_signal(fetch_setting::<Difficulty>("difficulty").unwrap_or_default());
     let (size, set_size) = create_signal(fetch_setting::<Size>("size").unwrap_or_default());
@@ -31,65 +36,68 @@ pub fn HomePage() -> impl IntoView {
     let difficulty_ref = create_node_ref::<html::Select>();
     let size_ref = create_node_ref::<html::Select>();
 
+    let username_error_action = create_action(move |&()| async move {
+        let username_input = username_ref.get().expect("noderef assigned");
+
+        let name_input = username_input.prop(
+            "style",
+            "
+            border-color: red;
+        ",
+        );
+
+        let error_span = error_ref.get().expect("noderef assigned");
+
+        let error_span = error_span.prop(
+            "style",
+            "
+            visibility: visible;
+            opacity: 1;
+            transition: opacity .2s linear;
+        ",
+        );
+
+        TimeoutFuture::new(500).await;
+
+        name_input.prop("style", "");
+
+        TimeoutFuture::new(2000).await;
+
+        error_span.prop(
+            "style",
+            "
+            visibility: hidden;
+            opacity: 0;
+            transition: visibility 0s .2s, opacity .2s linear;
+        ",
+        );
+    });
+
     let on_username_input = move |ev| {
         let old_name = username();
         let new_name = event_target_value(&ev);
         if new_name.len() <= *USERNAME_BOUNDS.end() && valid_chars(&new_name) {
-            set_username(new_name);
+            set_username(Some(new_name));
         } else {
             set_username(old_name);
-
-            create_action(move |&()| async move {
-                let username_input = username_ref.get().unwrap();
-
-                let name_input = username_input.prop(
-                    "style",
-                    "
-                    border-color: red;
-                ",
-                );
-
-                let error_span = error_ref.get().unwrap();
-
-                let error_span = error_span.prop(
-                    "style",
-                    "
-                    visibility: visible;
-                    opacity: 1;
-                    transition: opacity .2s linear;
-                ",
-                );
-
-                TimeoutFuture::new(500).await;
-
-                name_input.prop("style", "");
-
-                TimeoutFuture::new(2000).await;
-
-                error_span.prop(
-                    "style",
-                    "
-                    visibility: hidden;
-                    opacity: 0;
-                    transition: visibility 0s .2s, opacity .2s linear;
-                ",
-                );
-            })
-            .dispatch(());
+            username_error_action.dispatch(());
         }
     };
 
     let on_settings_submit = move |ev: ev::SubmitEvent| {
-        let username = username_ref.get().unwrap().value();
-        if USERNAME_BOUNDS.contains(&username.len()) && valid_chars(&username) {
-            apply_setting("username", &username);
-            set_username(username);
+        let provided_username = username_ref.get().expect("noderef assigned").value();
+        if USERNAME_BOUNDS.contains(&provided_username.len()) && valid_chars(&provided_username) {
+            if !username().is_some_and(|username| username == provided_username) {
+                apply_setting("username", &provided_username);
+                set_username(Some(provided_username));
+            }
         } else {
             ev.prevent_default();
+            username_error_action.dispatch(());
             return;
         }
 
-        let difficulty_select = difficulty_ref.get().unwrap();
+        let difficulty_select = difficulty_ref.get().expect("noderef assigned");
         if let Ok(selected_difficulty) = difficulty_select.value().parse() {
             if difficulty() != selected_difficulty {
                 apply_setting("difficulty", &selected_difficulty);
@@ -100,7 +108,7 @@ pub fn HomePage() -> impl IntoView {
             return;
         }
 
-        let size_select = size_ref.get().unwrap();
+        let size_select = size_ref.get().expect("noderef assigned");
         if let Ok(selected_size) = size_select.value().parse() {
             if size() != selected_size {
                 apply_setting("size", &selected_size);
@@ -132,11 +140,19 @@ pub fn HomePage() -> impl IntoView {
                             <input
                                 type="text"
                                 name="username"
-                                prop:value=username
+                                prop:value=move || {
+                                    username().unwrap_or_else(random_name)
+                                }
                                 size="12"
                                 node_ref=username_ref
                                 on:input=on_username_input
                             />
+                            <span
+                                class="random-name"
+                                on:click=move |_| {
+                                    username_ref.get().expect("noderef assigned").set_value(&random_name());
+                                }
+                                inner_html=DICE_SVG/>
                             <div class="username-error-container">
                                 <span class="username-error" node_ref=error_ref>
                                     "Name must be 3-10 alphanumeric characters and underscores"
